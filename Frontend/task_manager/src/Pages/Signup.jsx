79cf569ed93001012c6b97b71ea9c09de659fe88
merytpeters <merytpeters@gmail.com>
2025-05-06 18:24:20 +0100

    feat(signup): implement user signup page UI and form handling
    - Created input fields for user details and connected form to state.
    
    - Added basic validation, form submission handler, and styled layout.

---
+++ Frontend/task_manager/src/Pages/Signup.jsx
@@ -0,0 +1,110 @@
+import { useState } from 'react';
+import axios from 'axios';
+import Navbar from '../Components/navbar.jsx';
+import { useNavigate } from 'react-router-dom';
+import '../styles/signup.css';
+
+function Signup() {
+    const [formData, setFormData] = useState ({
+        first_name: '',
+        last_name: '',
+        email: '',
+        password: '',
+        username:'',
+        confirmPassword: ''
+    });
+
+    // Handles input
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
+    // Handles Submit
+    const navigate = useNavigate();
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = axios.post('http://localhost:3000/api/users/signup', formData)
+            console.log("User Created:", response.data)
+            alert("Signup Successful");
+            navigate('/login');
+        } catch (error) {
+            console.error("Error creating user:", error);
+        }
+
+        if (formData.password !== formData.confirmPassword) {
+            alert ("Passwords do not match");
+            return;
+        }
+    };
+        
+    return (
+        <div className="signup">
+            <Navbar />
+            <p className='signup-header'>SignUp</p>
+            <form className="form" onSubmit={handleSubmit}>
+                <div>
+                    <input
+                      type="text"
+                      name="first_name"
+                      value={formData.first_name}
+                      onChange={handleChange}
+                      placeholder="First Name"
+                    />
+                </div>
+                <div>
+                    <input
+                      type="text"
+                      name="last_name"
+                      value={formData.last_name}
+                      onChange={handleChange}
+                      placeholder="Last Name"
+                    />
+                </div>
+                <div>
+                    <input
+                      type="text"
+                      name="username"
+                      value={formData.username}
+                      onChange={handleChange}
+                      placeholder="Username"
+                    />
+                </div>
+                <div>
+                    <input
+                      type="email"
+                      name="email"
+                      value={formData.email}
+                      onChange={handleChange}
+                      placeholder="Email"
+                    />
+                </div>
+                <div>
+                    <input
+                      type="password"
+                      name="password"
+                      value={formData.password}
+                      onChange={handleChange}
+                      placeholder="Password"
+                    />
+                </div>
+                <div>
+                    <input
+                      type="password"
+                      name="confirmPassword"
+                      value={formData.confirmPassword}
+                      onChange={handleChange}
+                      placeholder="Confirm password"
+                    />
+                </div>
+                <button type='submit' className='signup-button'>Signup</button>
+            </form>
+        </div>
+    )
+}
+
+export default Signup;
