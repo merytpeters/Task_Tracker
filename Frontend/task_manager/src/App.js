6fc37d0c1f475c8b21e1c4ec3e44d9228ad965b6
merytpeters <merytpeters@gmail.com>
2025-05-08 14:56:25 +0100

    Chore(UI): Imported App.css

--- Frontend/task_manager/src/App.js
+++ Frontend/task_manager/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Home from './Pages/Home';
 import Signup from './Pages/Signup';
 import Login from './Pages/Login';
+import './App.css';
 
 function App() {
   return (
