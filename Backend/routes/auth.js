23968e451c9d9a9d4dd89d5f2fda17baf04f4104
merytpeters <merytpeters@gmail.com>
2025-08-22 13:37:27 +0100

    Refactor: Update Google login route to remove API prefix

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -3,6 +3,6 @@ import { googleLogin } from '../controllers/authController.js'
 
 const router = express.Router();
 
-router.post('/api/auth/google', googleLogin);
+router.post('/google', googleLogin);
 
 export default router
